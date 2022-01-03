1f0a5aeb66c335a3ec2296c54d662baf853c3386
Cristian Vázquez <cristianvaz81993@gmail.com>
2022-01-03 09:46:35 +0100

    Bringup Kraken

--- device.mk
+++ device.mk
@@ -16,7 +16,6 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 # Pocket Mode
 PRODUCT_PACKAGES += \
